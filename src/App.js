72cf0182d3975ba9d9a9483d1f4df4c920edfff4
Steven Wu <steven.wu@flinders.edu.au>
2017-03-08 14:13:04 +1030

    Implement new components 'ToDoList' and 'GitHubUser'.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
@@ -22,10 +23,72 @@ class Button extends Component {
     }
 }
 
+class GitHubUser extends Component {
+
+    state = {
+        email: ''
+    }
+
+    componentDidMount() {
+        axios.get('https://api.github.com/users/shadeven')
+            .then(response => {
+                this.setState({email: response.data.email})
+
+                for (var item in response.data) {
+                    if (response.data.hasOwnProperty(item)) {
+                        // console.log(item + ": " + response.data)
+                    }
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <div>{this.state.email}</div>
+        )
+    }
+}
+
+class ToDoList extends Component {
+
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                 ))}
+            </ul>
+        )
+    }
+}
+
 class App extends Component {
 
     state = {
-        count: 0
+        count: 0,
+        items: [],
+        text: ''
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState((prevState) => ({
+            items: prevState.items.concat(newItem),
+            text: ''
+        }));
+    }
+
+    handleChange(e) {
+        this.setState({
+            text: e.target.value
+        })
     }
 
     handleIncrement() {
@@ -50,6 +113,13 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
+                <ToDoList items={this.state.items}/>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.text}/>
+                    <button>Add</button>
+                </form>
+
+                <GitHubUser/>
                 <Counter count={this.state.count}/>
                 <Button onClick={() => this.handleIncrement()} title="INCREMENT"/>
                 <Button onClick={() => this.handleDecrement()} title="DECREMENT"/>
