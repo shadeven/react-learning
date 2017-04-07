03447696d17a346a00fbe436d2d7ac75a0b7044e
Steven Wu <steven.wu@flinders.edu.au>
2017-04-07 14:26:10 +0930

    Add React Bootstrap table.

--- src/components/CreateGoal.js
+++ src/components/CreateGoal.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../App.css';
 
 export default class CreateGoal extends Component {
 
     render() {
         return (
-            <div>
-                <div className="row">
-                    <div className="col-md-3">
-                        <form>
-                            <div className="input-group">
-                                <input onChange={this.props.onChange} type="text" className="form-control"/>
-                                <span className="input-group-btn">
-                                <button className="btn btn-primary" type="button"
-                                        onClick={this.props.onClick}>Create</button>
-                            </span>
+            <div className="container">
+                <div className="col-md-6 col-md-offset-3">
+                    <div className="form-area">
+                        <form role="form">
+                            <h3>Create Goal</h3>
+                            <div className="form-group">
+                                <input type="text" className="form-control" id="name" name="name"
+                                       placeholder="Goal Name" value={this.props.goalName}
+                                       onChange={this.props.onNameChange}/>
                             </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control" id="description" name="description"
+                                       placeholder="Description" value={this.props.descriptionName}
+                                       onChange={this.props.onDescriptionChange}/>
+                            </div>
+                            <button type="button" id="submit" name="submit" className="btn btn-primary pull-right"
+                                    onClick={this.props.onClick}>Submit
+                            </button>
                         </form>
                     </div>
                 </div>
--- src/components/GoalContainer.js
+++ src/components/GoalContainer.js
@@ -1,12 +1,14 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import Goal from './Goal';
 import CreateGoal from "./CreateGoal";
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import 'react-bootstrap-table/css/react-bootstrap-table.css';
 
 export default class GoalContainer extends Component {
 
     state = {
         name: '',
+        description: '',
         goals: []
     }
 
@@ -25,29 +27,48 @@ export default class GoalContainer extends Component {
     handleClick(e) {
         e.preventDefault();
         axios.post('http://127.0.0.1:8000/api/createGoal', {
-            name: this.state.name
+            name: this.state.name,
+            description: this.state.description
         })
             .then(response => {
-                this.setState((prevState) => ({
-                    goals: prevState.goals.concat(response.data)
-                }));
+                this.setState({
+                    name: '',
+                    description: ''
+                })
+
+                this.setState((prevState) => {
+                    return {goals: prevState.goals.concat(response.data)};
+                });
             })
             .catch(error => {
                 console.log(error);
             })
     }
 
-    handleChange(e) {
+    handleNameChange(e) {
         this.setState({
             name: e.target.value
+        })
+    }
+
+    handleDescriptionChange(e) {
+        this.setState({
+            description: e.target.value
         })
     }
 
     render() {
         return (
             <div>
-                <CreateGoal onChange={(e) => this.handleChange(e)} onClick={(e) => this.handleClick(e)}/>
-                <Goal goals={this.state.goals}/>
+                <CreateGoal goalName={this.state.name} descriptionName={this.state.description}
+                            onNameChange={(e) => this.handleNameChange(e)}
+                            onDescriptionChange={(e) => this.handleDescriptionChange(e)}
+                            onClick={(e) => this.handleClick(e)}/>
+                <BootstrapTable data={this.state.goals} striped hover condensed>
+                    <TableHeaderColumn dataField="id" width='5%' isKey={true}>ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField="name">Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="description">Description</TableHeaderColumn>
+                </BootstrapTable>
             </div>
         )
     }
